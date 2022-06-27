d3845253466cd85f110a314f9b3f11968bfd7e12
Web worker <briannyaberi1@gmail.com>
2022-06-28 01:10:31 +0300

    Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-let age = 20;
-let sale = 'paid'
-let level
-
-// avoid any data type, as it loses all typescript necesity
-function render(document) {
-    console.log(document);
-    
-}
+let numbers: number[] = [];
