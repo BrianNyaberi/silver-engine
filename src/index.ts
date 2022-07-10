e885dcefb3f9575bab6119e5e1a0a14121e15e54
Web worker <briannyaberi1@gmail.com>
2022-07-10 23:09:05 +0300

    Signed-off-by: Web worker <briannyaberi1@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // Enums
 
-const small = 1
-const medium = 1
-const large = 1
+// const small = 1
+// const medium = 2
+// const large = 3
+
  // instead of the above,venums come in
+enum Size {small = 1, medium, large};
+let mySize: Size = Size.medium
