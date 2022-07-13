0f74ad9017aa166528aabfaa9837bc35878429a1
Web worker <briannyaberi1@gmail.com>
2022-07-13 23:50:17 +0300

    Signed-off-by: Web worker <briannyaberi1@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 // Union types
 
 function kgToLbs(weight: number | string): number {
-    weight
+     // Narrowing
+     if (typeof weight === 'number') {
+        return weight * 2.2
+     } else {
+        return parseInt(weight) * 2.2
+     } 
 }
 
 kgToLbs(10);
