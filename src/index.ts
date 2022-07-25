27c8a40939d7c5136b2cb5cb5be8692cf79cf361
Web worker <briannyaberi1@gmail.com>
2022-07-25 21:52:13 +0300

    Combine two type using an intersection type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 // Intersection types (Combiningthis types)
 
 type Draggable = {
+   drag: () => void;
 
 }
 
 type Resiazble = {
+   resize: () => void;
 
 }
+
+// Combine them using an intersection type
+type UIWidget = Draggable & Resiazble;
+
+// let textBox: UIWidget = {
+//    drag () => {}
+// }
 
 
