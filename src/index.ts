0b1ed2a42afd5b79212a7f6e74b2df6660af2496
Web worker <briannyaberi1@gmail.com>
2022-07-26 10:34:15 +0300

    Last last: Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-// Nullable types (union operator)
-
-function greet(name: string | null | undefined) {
-    if (name) {
-        console.log(name.toLocaleUpperCase())
-    } else {
-        console.log('Hola, buenos dias?')
-    }
-  
-}
-
-// this will crash as we can 
+// Optional chaining 
